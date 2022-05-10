20c31a219f45dd9a15c0efb3a905e9fe6d229010
Casey Harding <casey.r.harding@gmail.com>
2022-05-10 13:17:08 -0400

    Through to stretch goal!

--- src/index.js
+++ src/index.js
@@ -39,8 +39,10 @@ launchButton.addEventListener("click", function(evt) {
 // 👉 TASK 3- Create a function that launches!
 // It should open the confirmation modal.
 // Add it as an event listener for click events on the launch button.
-function launch(evt) {
+function launch() {
   modal.classList.remove("off");
+  successMessage.classList.add("off");
+  failureMessage.classList.add("off");
 }
 launchButton.addEventListener("click", launch);
 
@@ -60,7 +62,7 @@ launchButton.addEventListener("click", launch);
 // 👉 TASK 4- Create a function to confirm the launch.
 // It should close the modal and display a success report.
 // Add it as a listener for clicks on the confirmation button.
-function confirm(evt) {
+function confirm() {
   modal.classList.add("off");
   successMessage.classList.remove("off");
 }
@@ -79,7 +81,12 @@ cancelButton.addEventListener("click", cancel);
 // the user hits the Escape key on their keyboard.
 // Add it as an event listener for 'keydown' events on document.
 
-
+function escKey(event) {
+  if (event.key === "Escape") {
+    modal.classList.add("off");
+  }
+}
+document.addEventListener("keydown", escKey);
 // 👉 TASK 7- Add to ALL ELEMENTS ON THE PAGE an event listener for click events.
 // It should console.log the target 🎯 of the event.
 // It should also console.log the CURRENT target 🧭 of the event.
